2f9d1977211e5e9727a35490f177311303b7f31e
Alinichenko <a.a.alinichenko@gmail.com>
2019-04-17 11:39:55 +0000

    cloud-testapp-v8

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/a_a_alinichenko_otus/
 git clone -b monolith https://github.com/express42/reddit.git
-cd /home/a_a_alinichenko_otus/reddit && bundle install
+cd /home/a_a_alinichenko_otus/reddit
+bundle install
 puma -d
 
